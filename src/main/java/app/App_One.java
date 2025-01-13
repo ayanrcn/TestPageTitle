7b78ce05da1b88887804a5eb54d368568af7f7b0
Ayan Banerjee <ayanbanerjee962@gmail.com>
2025-01-13 22:12:32 +0530

    chrome driver updated again with \

--- src/main/java/app/App_One.java
+++ src/main/java/app/App_One.java
@@ -5,7 +5,7 @@ public class App_One {
 	private WebDriver driver;
 		// TODO Auto-generated method stub
 	public App_One() {
-		System.setProperty("webdriver.chrome.driver","C:/Users/Ayan Banerjee/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Ayan Banerjee\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		this.driver = new ChromeDriver();
 	}
 		//System.out.println("Helo this is my first maven app");
